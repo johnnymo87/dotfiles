e2ccd9a1561bfad67b0359eb345d038d40d4b9fa
johnnymo87 <johnnymo87@gmail.com>
2014-07-08 15:30:38 +0100

    adds ruby file line count alias

--- zsh/zsh-aliases.zsh
+++ zsh/zsh-aliases.zsh
@@ -9,6 +9,8 @@ alias -g N="| /dev/null"
 alias -g S='| sort'
 alias -g G='| grep' # now you can do: ls foo G something
 
+alias -g rbc="find . -name '*.rb' | xargs wc -l | sort -n | tail -10"
+
 # Functions
 #
 # (f)ind by (n)ame
