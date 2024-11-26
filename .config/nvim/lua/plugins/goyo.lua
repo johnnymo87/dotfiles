b6056db60a062e2b27524dc312ccc3bd56befaf1
Jonathan Mohrbacher <johnnymo87@gmail.com>
2024-11-25 22:48:00 -0500

    Add goyo for nicely squeezed margins for reading

---
+++ .config/nvim/lua/plugins/goyo.lua
@@ -0,0 +1,7 @@
+-- Advertised as "Distraction-free writing in Vim", I like Goyo for its ability
+-- to offer nicely squeezed margins, which I want when reading text.
+
+return {
+  "junegunn/goyo.vim",
+  cmd = "Goyo", -- lazy-load on a command
+}
